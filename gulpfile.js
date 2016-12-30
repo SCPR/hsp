ac3d1916b62705057bba493b521fc2a4d8790e39
Ben Titcomb <btitcomb@scpr.org>
2016-12-30 12:19:08 -0800

    Add some compilation tasks to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,14 @@ const gulp         = require('gulp'),
       browserify   = require('browserify'),
       source       = require('vinyl-source-stream'),
       babel        = require('babelify'),
-      browserSync  = require('browser-sync').create();
+      browserSync  = require('browser-sync').create(),
+      md5          = require('gulp-md5-plus'),
+      clean        = require('gulp-clean'),
+      cleanCSS     = require('gulp-clean-css'),
+      uglify       = require('gulp-uglify');
 
 
-gulp.task('sass', () => {
+gulp.task('build:sass', () => {
   return gulp.src('./app/styles/index.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
@@ -17,12 +21,8 @@ gulp.task('sass', () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', () => {
-  gulp.watch('./app/styles/*.sass', ['sass']);
-});
-
-gulp.task('browserify', () => {
-  browserify('./app/javascripts/index.js', {debug: true, extensions: ['.js']})
+gulp.task('build:js', () => {
+  return browserify('./app/javascripts/index.js', {debug: true, extensions: ['.js']})
     // .transform('browserify-shim')
     .transform('brfs')
     .transform('babelify', {presets: ['es2015']})
@@ -32,17 +32,42 @@ gulp.task('browserify', () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task('build', ['sass', 'browserify']);
+gulp.task('build', ['build:sass', 'build:js']);
 
-gulp.task('serve', ['sass', 'browserify'], () => {
+gulp.task('serve', ['build:sass', 'build:js'], () => {
   browserSync.init({
     server: "./"
   });
-
-  gulp.watch('./app/styles/*.sass', ['sass']);
-  gulp.watch('./app/javascripts/*.js', ['browserify']);
+  gulp.watch('./app/styles/*.sass', ['build:sass']);
+  gulp.watch('./app/javascripts/*.js', ['build:js']);
   gulp.watch('./app/images/*.svg').on('change', browserSync.reload);
   gulp.watch('./index.html').on('change', browserSync.reload);
 })
 
-gulp.task('default', ['build']);
+gulp.task('default', ['serve']);
+
+gulp.task('compile:html', () => {
+  return gulp.src('./index.html')
+    .pipe(gulp.dest('./build')); 
+})
+
+gulp.task('compile:css', () => {
+  return gulp.src(['./index.css'])
+    .pipe(md5(10, './build/index.html'))
+    .pipe(cleanCSS({compatibility: 'ie11'}))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('compile:js', () => {
+  return gulp.src(['./index.js'])
+    .pipe(md5(10, './build/index.html'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('clean', () => {
+  return gulp.src('./build', {read: false})
+    .pipe(clean({force: true}));
+});
+
+gulp.task('compile', ['build', 'clean', 'compile:html', 'compile:css', 'compile:js']);
