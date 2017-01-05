bcb54ca5c7eb92ff94fd87d5d2a418d45e180cb0
Ben Titcomb <btitcomb@scpr.org>
2017-01-05 09:54:16 -0800

    Track index.js.

---
+++ app/scripts/index.js
@@ -0,0 +1,10 @@
+'use strict';
+window.jQuery = require('jquery');
+window.$      = jQuery;
+require('./header');
+require('./player');
+require('./feedback');
+require('./geocomplete');
+require('./lookup')('#page-form');
+require('./lookup')('#draw-form');
+require('./lookup-link');
